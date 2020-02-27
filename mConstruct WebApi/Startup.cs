ef558e7b139b0c15c9cfc447d51c821529113d6e
Aman Saryal <amansaryal@gmail.com>
2020-02-27 15:53:31 +0530

    Environment specific appsettings files added. Startup configured.

--- mConstruct WebApi/Startup.cs
+++ mConstruct WebApi/Startup.cs
@@ -1,27 +1,29 @@
-﻿using System.Linq;
-using System.Reflection;
+﻿using mConstruct_WebApi.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.OpenApi.Models;
-using MobileWebApiLibrary.Action_Filters;
+using Microsoft.Extensions.Logging;
 using MobileWebApiLibrary.Formatters;
 using MobileWebApiLibrary.Middlewares;
-using Swashbuckle.AspNetCore.SwaggerGen;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.ObjectPool;
+using Newtonsoft.Json;
+using System.Buffers;
+using mConstruct_WebApi.Middlewares;
 
 namespace mConstruct_WebApi
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration, IHostingEnvironment env)
-        {
-            _currentEnvironment = env;
-            Configuration = configuration;
-        }
+        private readonly ILogger<Startup> logger;
 
-        private readonly IHostingEnvironment _currentEnvironment;
+        public Startup(IConfiguration configuration, ILogger<Startup> logger)
+        {
+            Configuration = configuration;          
+            this.logger = logger;
+        }
 
         public IConfiguration Configuration { get; }
 
@@ -31,42 +33,30 @@ namespace mConstruct_WebApi
             services.AddMvc(
                 options =>
                 {
-                    options.Filters.Add(new HttpResponseExceptionFilter(_currentEnvironment));
+
+                    //init JsonInputFormatter
+
+                    ObjectPoolProvider _objectPoolProvider = new DefaultObjectPoolProvider();
+                    JsonSerializerSettings _serializerSettings = new JsonSerializerSettings();
+                    var myFormatter = new MyJsonInputFormatter(
+                        logger,
+                        _serializerSettings,
+                        ArrayPool<char>.Shared,
+                        _objectPoolProvider,
+                        new MvcOptions(),
+                        new MvcJsonOptions());
+
+                    options.InputFormatters.Insert(1, myFormatter);
                     options.InputFormatters.Add(new ProtobufInputTypeFormatter());
                     options.OutputFormatters.Add(new ProtobufOutputTypeFormatter());
                     options.OutputFormatters.Add(new ProtoJsonOutputTypeFormatter());
                 }
                 ).SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
-            //api versioning conflicts with exception handler routes, thus disabling any and all kinds of global exception handling. hence it is disabled.
-            //use explicit versioning in controller routes
-            //but use the ApiVersioningAttribute to generate swagger doc as shown below
-            //services.AddApiVersioning(); 
+            services.AddApiVersioning();
 
-            services.AddSwaggerGen(options =>
-            {
-                options.SwaggerDoc("v1", new OpenApiInfo { Title = "Session Api v1", Version = "v1" });
-                options.SwaggerDoc("v2", new OpenApiInfo { Title = "Session Api v2", Version = "v2" });
-
-                //// This call removes version from parameter, without it we will have version as parameter 
-                //// for all endpoints in swagger UI
-                //options.OperationFilter<RemoveVersionFromParameter>(); //use when api versioning is enabled
-
-                //// This replaces v{version:apiVersion} to real version of corresponding swagger doc.
-                //options.DocumentFilter<ReplaceVersionWithExactValueInPath>(); //use when api versioning is enabled
-
-                ////This tells swagger what all actions are to be shown in the given version
-                options.DocInclusionPredicate((docName, apiDesc) =>
-                {
-                    if (!apiDesc.TryGetMethodInfo(out MethodInfo methodInfo)) return false;
-
-                    var versions = methodInfo.DeclaringType
-                        .GetCustomAttributes(true)
-                        .OfType<ApiVersionAttribute>()
-                        .SelectMany(attr => attr.Versions);
-
-                    return versions.Any(v => $"v{v.ToString()}" == docName);
-                });
+            services.AddDbContext<PlngDbContext>(optionsAction => {
+                optionsAction.UseOracle(Configuration.GetConnectionString("PlanningDatabase"));
             });
 
         }
@@ -76,28 +66,29 @@ namespace mConstruct_WebApi
         {
             if (env.IsDevelopment())
             {
-                //app.UseDeveloperExceptionPage();
-                app.UseExceptionHandler("/error");
+
             }
             else
             {
                 app.UseHsts();
-                app.UseExceptionHandler("/error");
+
             }
+
+            if (env.IsEnvironment("Local"))
+                app.UsePathBase("/mConstructApi");
+
+            app.UseExceptionHandler("/error");
 
             app.UseHttpsRedirection();
 
+            app.UseMiddleware<AuthenticationMiddleware>();        
+
             app.UseMiddleware<SecurityMiddleware>();
+
+            app.UseMiddleware<MyExceptionHandlerAndLoggerMiddleware>();
 
             app.UseMvc();
 
-            app.UseSwagger();
-
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint($"/swagger/v1/swagger.json", $"v1");
-                c.SwaggerEndpoint($"/swagger/v2/swagger.json", $"v2");
-            });
         }
     }
 }
